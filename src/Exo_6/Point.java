e2beaa624fe47d373e12e00f560d3685ad5cdd0f
oussa29-dev <ko_benkhaoua@esi.dz>
2025-08-03 07:41:18 +0100

    modify

---
+++ src/Exo_6/Point.java
@@ -0,0 +1,20 @@
+class Point {
+
+    private int x;
+    private int y;
+
+    public Point(int absc, int ord) {
+        x = absc;
+        y = ord;
+    }
+
+    public void deplacer(int dx, int dy) {
+        x = x + dx;
+        y = y + dy;
+    }
+
+    public void afficher() {
+        System.out.println("Mes coordonnées sont x = " + x + " et y =" + y);
+    }
+
+}
